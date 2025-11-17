a5eea37445d48b8a11154db74a06ca41c163cf19
xuxife <xuxife@gmail.com>
2025-11-17 22:37:31 +1100

    add median

---
+++ median1/median.go
@@ -0,0 +1,5 @@
+package median1
+
+import "github.com/xuxife/gazelle-multi-gomod-poc/dep"
+
+var Version = "median1/" + dep.Version
---
+++ median2/median.go
@@ -0,0 +1,5 @@
+package median2
+
+import "github.com/xuxife/gazelle-multi-gomod-poc/dep"
+
+var Version = "median2/" + dep.Version
